bb9a52e6baa6d2499b62fbc53bd34857c3c36ea9
john-h-celeste <rvail8330@floridapoly.edu>
2025-05-12 17:59:10 +0000

    Initial commit

---
+++ week1/01/sample.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using std:cout;
+using std::endl
+
+int main()
+
+  / The result of computation of
+  //(10 + 123) * 7/(23 - 20 - 3) is
+  cout << (10 + 123) * 7/(23 - 20 - 3) << endl;
+
+  // Display the cube of 2
+  cout << "Cube of 2 is " << 2 * 3 << cout::endl;
+  return 0;
+}
---
+++ week1/01/sample_fixed.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main() {
+    // The result of computation of
+    // (10 + 123) * 7 / (23 - 20 - 3) is a runtime error
+    //cout << (10 + 123) * 7 / (23 - 20 - 3) << endl;
+
+    // Display the cube of 2
+    cout << "Cube of 2 is " << 2 * 2 * 2 << endl;
+
+    return 0;
+}
